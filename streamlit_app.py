a7dc8c3e53835dfd25a0d10e63e857d7ab430dbf
anggunecy88 <nurazizahanggun33@gmail.com>
2025-05-27 09:48:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ st.write(
 st.image("Unknown 21.jpeg", width=200)
 st.image("Paul mescal and Daisy edgar jones.jpeg", width=200)
 
+
 st.title("How i'm with her is my normal personality.")
 st.header("I love Normal People & Coffe)  
 angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
