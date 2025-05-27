63e86f08dfe8fe3b7662fbb9cde42def217da744
anggunecy88 <nurazizahanggun33@gmail.com>
2025-05-27 09:44:57 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,13 @@ st.write(
    "Brewed from darkness, coffee teaches us how to turn bitter into"
 )
 st.image("Unknown 21.jpeg", width=200)
+st.image("Paul mescal and Daisy edgar jones.jpeg", width=200)
+
+st.title("How i'm with her is my normal personality.")
+st.header("I love Normal People & Coffe)  
+angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
+
+if (angka % 2) == 0:
+  st.write(f"{angka} adalah Bilangan Genap")
+else:
+  st.write(f"{angka} adalah Bilangan Ganjil")
