1271f3acb48b48de2d664c9eb56107356f382650
anggunecy88 <nurazizahanggun33@gmail.com>
2025-05-27 09:02:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("☕️ I Love Coffe")
 st.write(
-   "Brewed from darkness, coffee teaches us how to turn bitter into bold." "[docs.streamlit.io](https://docs.streamlit.io/)."
+   "Brewed from darkness, coffee teaches us how to turn bitter into"
 )
